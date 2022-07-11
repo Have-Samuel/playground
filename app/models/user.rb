cca0da69f5ac86a771d9f9140cc9906e45a2587d
Have Samuel <samiehave9@gmail.com>
2022-07-12 00:30:53 +0300

    Created the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :posts
+  has_many :likes
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def last_3_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
