2de8cfaf405e02c4e3dc48d9a09a5891d8c1916d
Have Samuel <samiehave9@gmail.com>
2022-07-12 00:49:03 +0300

    Create comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :text, presence: true, length: { minimum: 3, maximum: 255 }
+
+  def update_comments_counter
+    post.comments_counter = 0 if post.comments_counter.nil?
+    post.increment!(:comments_counter)
+  end
 end
