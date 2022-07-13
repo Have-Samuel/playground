22a546ea98bc7fec046b7fe6edf0f5bcd565f829
Have Samuel <samiehave9@gmail.com>
2022-07-13 10:19:34 +0300

    Fix Linters

--- db/migrate/20220711205356_create_users.rb
+++ db/migrate/20220711205356_create_users.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :title
       t.string :photo
       t.text :bio
-      t.bigint :posts_counter, default:0
+      t.bigint :posts_counter, default: 0
 
       t.timestamps
     end
--- db/migrate/20220711215840_rename_user_column_title_to_name.rb
+++ db/migrate/20220711215840_rename_user_column_title_to_name.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameUserColumnTitleToName < ActiveRecord::Migration[7.0]
   def change
     rename_column :users, :title, :name
