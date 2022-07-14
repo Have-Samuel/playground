645ea20ad8be6fa97975099563ff8e7e57344d7d
Have Samuel <samiehave9@gmail.com>
2022-07-14 21:30:58 +0300

    Making changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :comments
-  has_many :posts, foreign_key: :author_id
+  has_many :posts, foreign_key: :user_id
   has_many :likes
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
