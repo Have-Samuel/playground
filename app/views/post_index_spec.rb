dd5c5672dcfdf9e302aacdf5d1d6597f0faffb0f
Have Samuel <samiehave9@gmail.com>
2022-07-20 19:47:39 +0300

    Created the post_index_spec.rb file

---
+++ app/views/post_index_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+Rspec.describe 'Post Index Page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Have', photo: 'https://example.com/photo.jpg', bio: 'Traveller')
+    @post = Post.create(user: @user, title: 'Post 1', text: 'Micronuat')
+    @comment = Comment.create(user: @user, post: @post, text: 'Traveller')
+    @like1 = Like.create(user: @user, post: @post)
+    @like2 = Like.create(user: @user, post: @post)
+    viait user_posts_path(@user)
+  end
+
+  it 'can see the user\'s profile pic' do
+    expect(page).to have_css('img')
+  end
+
+  it 'can see the user\'s name' do
+    expect(page).to have_content('Have')
+  end
+
+  it 'can see the number of posys each user has made' do
+    expect(page).to have_content('1')
+  end
+
+  it 'can see a post title' do
+    expect(page).to have_content('Post #1')
+  end
+
+  it 'can see some of the posts body' do
+    expect(page).to have_content('Traveller')
+  end
+
+  it 'can see the first comment' do
+    expect(page).to have_content('Traveller')
+  end
+
+  it 'can see the number of likes each post has' do
+    expect(page).to have_content('2')
+  end
+
+  it 'can see a section for pagination if there are more then five posts' do
+    expect(page).to have_content('Next')
+    expect(page).to have_content('Previous')
+  end
+end
