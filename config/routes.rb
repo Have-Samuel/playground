1bbc0445ea143474915e9baae7aec68277fbe700
Have Samuel <samiehave9@gmail.com>
2022-07-20 20:11:56 +0300

    Correcting the errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create, :new]
+    resources :posts, only: [:index, :show, :create]
     post 'posts/:id/comments' => 'posts#create_comment', as: :create_comment
     post 'post/:id/likes' => 'posts#create_like', as: :create_like
   end
