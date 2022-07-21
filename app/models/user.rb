74714c8cc87a81e4058fd98324d98ab620f13a59
Have Samuel <samiehave9@gmail.com>
2022-07-21 20:41:39 +0300

    Added some code to the user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   has_many :likes
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   def most_recent_three_posts
     posts.order(created_at: :desc).limit(3)
