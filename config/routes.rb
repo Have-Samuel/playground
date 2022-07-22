5c179863bf16eae2f999aa6bd058892ecaa9cae2
Have Samuel <samiehave9@gmail.com>
2022-07-22 15:13:58 +0300

    Edited the orutes and added the snippets.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#index'
+  root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create]
+    resources :posts, only: [:index, :show, :create, :destroy]
     post 'posts/:id/comments' => 'posts#create_comment', as: :create_comment
     post 'post/:id/likes' => 'posts#create_like', as: :create_like
   end
